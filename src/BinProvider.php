1d04f69b41dc4431891a5893bf78191de58a8770
Volokitin <ihor.volokitin@ab-soft.net>
2023-04-28 18:34:42 +0200

    Commission

--- src/BinProvider.php
+++ src/BinProvider.php
@@ -4,8 +4,10 @@ namespace App;
 
 class BinProvider
 {
+    private const BASE_BIN_ENDPOINT = 'https://lookup.binlist.net';
+
     public function lookupBin(string $bin)
     {
-        return file_get_contents('https://lookup.binlist.net/' . $bin);
+        return file_get_contents(self::BASE_BIN_ENDPOINT . '/' . $bin);
     }
 }
--- src/CurrencyRatesProvider.php
+++ src/CurrencyRatesProvider.php
@@ -4,9 +4,11 @@ namespace App;
 
 class CurrencyRatesProvider
 {
+    private const BASE_EXCHANGE_ENDPOINT = 'https://api.exchangeratesapi.io';
+
     public function getExchangeRate(string $currency)
     {
-        $content = file_get_contents('https://api.exchangeratesapi.io/latest');
+        $content = file_get_contents(self::BASE_EXCHANGE_ENDPOINT . '/latest');
 
         if ($content === null) {
             throw new \Exception("Can't get exchange rate");
